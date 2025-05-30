7dd878963f5a022badbdbab1570bbeb132412771
Personal <personal@Ankits-MacBook-Pro.local>
2025-05-30 21:53:37 +0530

    Adeed .env

--- src/lib/connectDb.js
+++ src/lib/connectDb.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully!');
+    console.log("MongoDB connected successfully!");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
